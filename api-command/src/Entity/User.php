51b81b86366b7f3ef3f69a14890160aad6ff50b4
Pascal Filipovicz <pascal.filipovicz@frozenk.net>
2018-11-21 12:10:50 +0100

    fix(docker): fix the rights on the /www/data folder

--- api-command/src/Entity/User.php
+++ api-command/src/Entity/User.php
@@ -18,8 +18,25 @@ class User
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
     }
 }
